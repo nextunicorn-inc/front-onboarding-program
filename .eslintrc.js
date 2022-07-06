28a06d7e1e528236241d5c728923a8e410df7f4a
Steven Semyung Oh <semyung.dev@gmail.com>
2022-07-06 16:33:57 +0900

    [CHORE] ESLint 환경 설정 변경

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,19 +23,5 @@ module.exports = {
     'import/prefer-default-export': 'off',
     'import/extensions': ['off'],
     'import/no-unresolved': ['off'],
-    'prettier/prettier': [
-      'error',
-      {
-        singleQuote: true,
-        semi: true,
-        useTabs: false,
-        tabWidth: 2,
-        trailingComma: 'all',
-        printWidth: 120,
-        bracketSpacing: true,
-        arrowParens: 'avoid',
-        endOfLine: 'auto',
-      },
-    ],
   },
 };
